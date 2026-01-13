296e865ed6550b6e20dcbf64c978211e98f3a716
nattawut.w <nattawut.w@b-connex.net>
2026-01-13 10:14:45 +0700

    fix no env mockup file

--- frontend/src/shared/handlers/api.handler.ts
+++ frontend/src/shared/handlers/api.handler.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, type AxiosRequestHeaders, type InternalAxiosRequestConfig } from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: import.meta.env.BASE_API || 'http://localhost:8080/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
