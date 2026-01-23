a71de4f0d0ef97e8e83533fad6e393caa1d9e110
nattawut.w <nattawut.w@b-connex.net>
2026-01-23 11:14:00 +0700

    feat(router): Implement routing with authentication and public access in App component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,77 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import LandingPage from './modules/post-login/core/pages/LandingPage';
-import PublicLayout from './modules/pre-login/core/components/PublicLayout';
-import Login from './modules/pre-login/login/pages/Login';
-import RequireAuth from './shared/guards/RequireAuth';
-import PrivateLayout from './modules/post-login/core/components/PrivateLayout';
-import Register from './modules/pre-login/login/pages/Register';
-import ForgotPassword from './modules/pre-login/login/pages/ForgotPassword';
-import PublicGuard from './shared/guards/PublicGuard';
-import NotFound from './modules/core/pages/NotFound';
-import Unauthorized from './modules/core/pages/Unauthorized';
-import { AuthProvider } from './shared/providers/AuthProvider';
 import { initTheme } from './shared/handlers/theme.handler';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <RequireAuth />,
-        children: [
-            {
-                element: <PrivateLayout />,
-                children: [
-                    {
-                        index: true,
-                        element: <LandingPage />,
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        path: '/pre',
-        element: <PublicGuard />,
-        children: [
-            {
-                element: <PublicLayout />,
-                children: [
-                    {
-                        index: true,
-                        // path: 'login',
-                        element: <Login />,
-                    },
-                    {
-                        path: 'register',
-                        element: <Register />,
-                    },
-                    {
-                        path: 'forgot-password',
-                        element: <ForgotPassword />,
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        path: '/unauthorized',
-        element: <Unauthorized></Unauthorized>,
-    },
-    {
-        path: '*',
-        element: <NotFound></NotFound>,
-    },
-]);
+import App from './App';
+import { AuthProvider } from './shared/providers/AuthProvider';
+// import store from './shared/redux/store';
+// import { Provider } from 'react-redux';
 
 initTheme();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <AuthProvider>
-            <RouterProvider router={router} />
-        </AuthProvider>
+        {/* <Provider store={store}> */}
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        {/* </Provider> */}
     </StrictMode>,
 );
